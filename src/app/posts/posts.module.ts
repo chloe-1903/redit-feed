c66669c34b372c8e841f5529ad774630148af3ac
chloe-1903 <chloe-1903@live.fr>
2020-07-14 22:01:58 +0200

    styling post entry

--- src/app/posts/posts.module.ts
+++ src/app/posts/posts.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { PageListComponent } from './components/page-list/page-list.component';
 import { PostEntryComponent } from './components/post-entry/post-entry.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 
 @NgModule({
   declarations: [PageListComponent, PostEntryComponent],
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatTooltipModule
   ]
 })
 export class PostsModule { }
