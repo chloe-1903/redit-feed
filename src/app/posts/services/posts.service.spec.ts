c6589eb1069212404726ef522de09eba5f7d8d5b
chloe-1903 <chloe-1903@live.fr>
2020-07-19 19:31:25 +0200

    testing get comments from service

--- src/app/posts/services/posts.service.spec.ts
+++ src/app/posts/services/posts.service.spec.ts
@@ -24,14 +24,29 @@ describe('PostsService', () => {
   it('correct sub should return posts', () => {
     service.getPosts('sweden', 10, null, null, null).subscribe((result) => {
       expect(result).toBeTruthy();
-      expect(result.postList.length).toBeGreaterThanOrEqual(1);
+      expect(result.itemsList.length).toBeGreaterThanOrEqual(1);
     });
   });
 
   it('not existing topic should not return posts', () => {
     service.getPosts('qsdfghj12LkX09', 10, null, null, null).subscribe((result) => {
       expect(result).toBeTruthy();
-      expect(result.postList.length).toBe(0);
+      expect(result.itemsList.length).toBe(0);
     });
   });
+
+  it('sticky rules post has comments', () => {
+    service.getComments('/r/sweden/comments/hqem4s/sweddit_universalis_europa_universalis_iv_mp/').subscribe((result) => {
+      expect(result).toBeTruthy();
+      expect(result.length).toBeGreaterThanOrEqual(1);
+    });
+  });
+
+  it('wrong permalink returns empty comment', () => {
+    service.getComments('/r/sweden/comments/hqem4s/unknown9875JGF9O01JGK0/').subscribe((result) => {
+      expect(result).toBeTruthy();
+      expect(result.length).toBe(0);
+    });
+  });
+
 });
