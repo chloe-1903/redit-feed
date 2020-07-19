70fc58b5127d00cc3fab16f5b46a4784d2e55910
chloe-1903 <chloe-1903@live.fr>
2020-07-19 14:50:55 +0200

    hover effect on cards

--- src/app/posts/components/post-entry/post-entry.component.ts
+++ src/app/posts/components/post-entry/post-entry.component.ts
@@ -16,7 +16,7 @@ export class PostEntryComponent implements OnInit {
   openDetails() {
     this.dialog.open(PostDetailsComponent, {
       data: this.post,
-      width: '60vw',
+      width: '70vw',
       autoFocus: false
     });
   }
